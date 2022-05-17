eed635e770bbf146a25721584cdd0f6a619a8cb6
liarboss <124275630@qq.com>
2022-05-17 10:06:37 +0800

    update

---
+++ boot-01-helloworld/src/main/java/com/example/boot01helloworld/bean/Pet.java
@@ -0,0 +1,20 @@
+package com.example.boot01helloworld.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 宠物
+ */
+@ToString
+@Data
+@NoArgsConstructor  //无参构造器
+@AllArgsConstructor  //全参构造器
+public class Pet {
+
+    private String name;
+
+}
---
+++ boot-01-helloworld/src/test/java/com/example/boot01helloworld/Boot01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot01helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
